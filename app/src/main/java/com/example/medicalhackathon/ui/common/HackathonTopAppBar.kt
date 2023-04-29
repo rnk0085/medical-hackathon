50932d3c66d48869fdd9659757af7aee56e65353
rnk0085 <rinawkmbi4k8@gmail.com>
2023-04-29 23:38:40 +0900

    feat: CenterAlignedTopAppBar を活用

--- app/src/main/java/com/example/medicalhackathon/ui/common/HackathonTopAppBar.kt
+++ app/src/main/java/com/example/medicalhackathon/ui/common/HackathonTopAppBar.kt
@@ -1,32 +1,40 @@
 package com.example.medicalhackathon.ui.common
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.primarySurface
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.medicalhackathon.ui.theme.MedicalHackathonTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HackathonTopAppBar(
     hackathonTopAppBarUiState: HackathonTopAppBarUiState,
 ) {
-    TopAppBar(
-        elevation = 4.dp,
+    CenterAlignedTopAppBar(
         title = {
-            // TODO: センタリングしたい
             Text(
                 text = hackathonTopAppBarUiState.title,
-                textAlign = TextAlign.Center,
+                color = Color.Black,
             )
         },
-        backgroundColor =  MaterialTheme.colors.primarySurface,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color.White,
+        ),
         navigationIcon = {
             hackathonTopAppBarUiState.navigationIcon?.let { topAppBarIcon ->
                 IconButton(onClick = topAppBarIcon.iconClicked) {
