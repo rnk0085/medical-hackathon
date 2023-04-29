d53c43ab302647cc5ae51180ae3535f5b6320708
rnk0085 <rinawkmbi4k8@gmail.com>
2023-04-29 22:47:02 +0900

    feat: HospitalDetailScreen のデザインを反映

---
+++ app/src/main/java/com/example/medicalhackathon/UtilColor.kt
@@ -0,0 +1,8 @@
+package com.example.medicalhackathon
+
+import androidx.compose.ui.graphics.Color
+
+object UtilColor {
+    val mainColor: Color = Color(0xFF47CEDB)
+    val originalGrayColor: Color = Color(0xFFC9C9C9)
+}
--- app/src/main/java/com/example/medicalhackathon/ui/reservation/hospitalList/HospitalDetailScreen.kt
+++ app/src/main/java/com/example/medicalhackathon/ui/reservation/hospitalList/HospitalDetailScreen.kt
@@ -1,28 +1,32 @@
 package com.example.medicalhackathon.ui.reservation.hospitalList
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.medicalhackathon.R
+import com.example.medicalhackathon.UtilColor.mainColor
+import com.example.medicalhackathon.UtilColor.originalGrayColor
 import com.example.medicalhackathon.ui.UtilText.hospitalAddress1
 import com.example.medicalhackathon.ui.UtilText.hospitalName1
 import com.example.medicalhackathon.ui.common.HackathonTopAppBar
@@ -33,6 +37,8 @@ import com.example.medicalhackathon.ui.theme.MedicalHackathonTheme
 fun HospitalDetailScreen(
     modifier: Modifier = Modifier,
 ) {
+    val startPadding = 20.dp
+
     Scaffold(
         topBar = {
             HackathonTopAppBar(
@@ -45,12 +51,19 @@ fun HospitalDetailScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPaddings)
-                .padding(horizontal = 10.dp),
+                .padding(innerPaddings),
         ) {
             item {
                 Text(
                     text = "所在地",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = startPadding,
+                            vertical = 24.dp,
+                        ),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
                 )
             }
 
@@ -66,12 +79,25 @@ fun HospitalDetailScreen(
             item {
                 Text(
                     text = "〒120-0015",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = startPadding,
+                            top = 24.dp,
+                        ),
                 )
             }
 
             item {
                 Text(
                     text = hospitalAddress1,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = startPadding,
+                            top = 12.dp,
+                            bottom = 36.dp,
+                        ),
                 )
             }
 
@@ -80,26 +106,53 @@ fun HospitalDetailScreen(
                     modifier = Modifier.fillMaxWidth(),
                     contentAlignment = Alignment.Center,
                 ) {
-                    Text(
-                        text = "初診で予約する",
-                        modifier = Modifier
-                            .size(height = 48.dp, width = 200.dp)
-                            .clip(
-                                RoundedCornerShape(16.dp)
-                            )
-                            .background(Color.Red)
-                            .clickable(
-                                onClick = { /* TODO */ }
-                            ),
-                        textAlign = TextAlign.Center,
-                    )
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.textButtonColors(
+                            backgroundColor = mainColor,
+                            contentColor = Color.White,
+                        ),
+                    ) {
+                        Text(
+                            text = "初診で予約する",
+                            modifier = Modifier
+                                .padding(horizontal = startPadding),
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Medium,
+                        )
+                    }
                 }
             }
 
             item {
-                Text(
-                    text = "再診で予約する"
-                )
+                Spacer(modifier = Modifier.height(12.dp))
+            }
+
+            item {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.textButtonColors(
+                            backgroundColor = originalGrayColor,
+                            contentColor = Color.White,
+                        ),
+                    ) {
+                        Text(
+                            text = "再診で予約する",
+                            modifier = Modifier
+                                .padding(horizontal = startPadding),
+                            textAlign = TextAlign.Center,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Medium,
+                        )
+                    }
+                }
             }
         }
     }
